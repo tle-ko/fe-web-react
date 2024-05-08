191918082cbdb9cb4562bbacfa8fe984e8d00b06
Whatdoyumin <lym4963@naver.com>
2024-05-08 19:25:33 +0900

    ✨ feat: mainHeader 컴포넌트 생성

---
+++ src/components/Header/mainHeader.jsx
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom';
+import logoWhite from '../../assets/images/logo-white.svg';
+
+export default function mainHeader() {
+  return(
+    <div className="w-screen h-16 top-0 left-0 fixed px-[120px] py-3
+      bg-color-blue-main flex flex-row gap-16 text-white text-xl">
+       <Link className="w-14" to="/"><img src={logoWhite} alt='' /></Link>
+       <div className="justify-start items-center gap-16 flex">
+        <Link className='font-cafe24' to="/problem">Problem</Link>
+        <Link className='font-cafe24' to="/crew">Crew</Link>
+       </div>
+    </div>
+  )
+}
