ef87d6319093cccfc32489e1c876da15fe042348
yoojin <everywavyday@gmail.com>
2024-05-13 00:10:35 +0900

    ✨ feat: footer 컴포넌트 수정

--- src/components/common/footer.jsx
+++ src/components/common/footer.jsx
@@ -1,24 +1,21 @@
 import { Link } from 'react-router-dom';
 import logoBlack from '../../assets/images/logo-black.svg';
 
-export default function footer() {
+export default function Footer() {
     return(
-<div className="w-[1680px] h-[105px] flex-col justify-start items-center inline-flex">
-    <div className="w-[1680px] h-[0px] border border-gray-400"></div>
-    <div className="p-6 justify-center items-center gap-[1003px] inline-flex">
+<div className="absolute mx-120 bottom-0 inline-flex">
+    <div className= "w-full px-12 pt-6 pb-12 border-t items-end border-gray-500 p-6 justify-between flex">
         <div className="flex-col justify-center items-start gap-1 inline-flex">
-            <div className="w-[350px] text-gray-500 text-sm font-semibold font-['Pretendard']">2024년 상명대학교 컴퓨터과학전공 캡스톤디자인 </div>
-            <div className="w-[366px] text-gray-500 text-sm font-medium font-['Pretendard']">코딩메리호 김동주 조유진 이유민 강윤진 김서영 민기홍</div>
+            <p className="text-gray-500 text-sm font-semibold">2024년 상명대학교 컴퓨터과학전공 캡스톤디자인</p>
+            <p className="text-gray-500 text-sm font-medium">코딩메리호 김동주 조유진 이유민 강윤진 김서영 민기홍</p>
         </div>
         <div className="flex-col justify-center items-end gap-1 inline-flex">
-            <div className="justify-start items-end gap-2 inline-flex">
-                <div className="w-[153px] text-center text-gray-500 text-base font-bold font-['Pretendard']">time limit exceeded</div>
-                <div className="w-[57.42px] h-9 pt-[0.04px] pb-[0.03px] justify-center items-center flex">
-                    <Link className="w-[57.42px] h-[35.93px]" to=""><img src={logoBlack} alt='' /></Link>
-                </div>
+            <div className="justify-start items-end gap-1 inline-flex">
+                <p className="text-gray-500 font-bold">time limit exceeded,</p>
+                <Link to="/"><img className="w-14" src={logoBlack} alt='' /></Link>
             </div>
-            <div className="text-center text-gray-500 text-sm font-semibold font-['Pretendard']">Copyright 2024. TLE. All rights reserved.</div>
-        </div>
+            <p className="text-gray-500 text-sm font-semibold">Copyright 2024. TLE. All rights reserved.</p>
+    </div>
     </div>
 </div>
     )
