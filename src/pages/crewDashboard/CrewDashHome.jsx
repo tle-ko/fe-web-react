eb8b560bab88c196179a6c4e4034a3117a86da82
Whatdoyumin <lym4963@naver.com>
2024-05-19 23:17:03 +0900

    ✨ feat: 크루 대시보드 홈 페이지 test

--- src/pages/crewDashboard/CrewDashHome.jsx
+++ src/pages/crewDashboard/CrewDashHome.jsx
@@ -1,5 +1,21 @@
-export default function crewDashHome(){
+import { useParams } from "react-router-dom"
+import SubHeader from "../../components/Header/subHeader"
+import SideNav from "../../components/nav/sideNav"
+
+export default function CrewDashHome(){
+  // url의 crewId값을 SubHeader에 보내주기 위해 id값 얻기
+  let { id } = useParams();
+  // SideNav에 들어갈 요소 리스트 넣기
+  // 아래는 예시로, 사용하는 nav 내용에 따라 리스트 변경하면 됨.
+  const elements = ["1주차", "2주차", "3주차", "4주차"]
+
   return(
-    <div></div>
+    <div>
+      <SubHeader crewId={ id } />
+      <div className="cardGrid2">
+        <SideNav elements={elements}/>
+        
+      </div>
+    </div>
   )
 }
