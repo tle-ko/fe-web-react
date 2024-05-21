87b91038885ed00f8a5f7a84eb3586e6191f2b27
Whatdoyumin <lym4963@naver.com>
2024-05-21 17:47:12 +0900

    💄 style: footer 컴포넌트 style 수정

--- src/components/common/footer.jsx
+++ src/components/common/footer.jsx
@@ -3,8 +3,8 @@ import logoBlack from '../../assets/images/logo-black.svg';
 
 export default function Footer() {
     return(
-<div className="absolute mx-120 bottom-0 inline-flex">
-    <div className= "w-full px-12 pt-6 pb-12 border-t items-end border-gray-500 p-6 justify-between flex">
+<div className="max-w-full mt-32 bg-color-bg left-0 ">
+    <div className= "max-w-full px-12 pt-6 pb-12 border-t items-end border-gray-500 p-6 justify-between flex">
         <div className="flex-col justify-center items-start gap-1 inline-flex">
             <p className="text-gray-500 text-sm font-semibold">2024년 상명대학교 컴퓨터과학전공 캡스톤디자인</p>
             <p className="text-gray-500 text-sm font-medium">코딩메리호 김동주 조유진 이유민 강윤진 김서영 민기홍</p>
