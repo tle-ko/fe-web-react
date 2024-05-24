99ff6854d2aaa4c9068af3f2aaa16e26687eb992
Whatdoyumin <lym4963@naver.com>
2024-05-24 18:35:50 +0900

    ✨ feat: 버튼 컴포넌트 수정

--- src/components/common/button.jsx
+++ src/components/common/button.jsx
@@ -1,14 +1,34 @@
 import React from 'react';
 
-const Button = ({ color, content, onClick }) => {
+const Button = ({ buttonSize, colorStyle, content, onClick }) => {
+  const styles = {
+    whiteBlack: "bg-gray-50 border border-gray-200 text-gray-600 text-sm hover:bg-gray-100",
+    redWhite: "bg-gray-50 border border-gray-200 text-rose-500 text-sm hover:bg-gray-100",
+    blueWhite: "bg-color-blue-main text-white text-sm hover:bg-color-blue-hover",
+    skyBlue: "bg-color-blue-w25 text-color-blue-main hover:bg-color-blue-w50",
+    grayWhite: "bg-gray-200 text-gray-50 hover:bg-gray-300",
+    detailBtn: "px-4 py-2 text-sm",
+    formBtn: "px-4 py-3 text-lg"
+  };
+
+  // 버튼 크기와 색상 스타일 결합
+  const buttonClass = `${styles[buttonSize]} ${styles[colorStyle]} rounded-lg flex justify-center items-center font-semibold`;
+
   return (
-    <button
-      className={`px-4 py-3 ${color} rounded-lg flex justify-center items-center text-gray-50 text-lg font-semibold`}
-      onClick={onClick}
-    >
+    <button className={buttonClass} onClick={onClick}>
       {content}
     </button>
   );
 }
 
 export default Button;
+
+/**
+ * usage
+ * <Button 
+      buttonSize="formBtn"
+      colorStyle="skyBlue"
+      content="로그인"
+      onClick={() => console.log('Button clicked')}
+    />
+ */
