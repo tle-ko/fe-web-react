40ce82af371d784bb4218e4aa63486bacbff116c
yoojin <everywavyday@gmail.com>
2024-05-28 16:23:54 +0900

    ✨ feat: ProblemMain 페이지 수정

--- src/pages/problemPage/ProblemMain.jsx
+++ src/pages/problemPage/ProblemMain.jsx
@@ -4,7 +4,11 @@ import useChildRoute from "../../hooks/useChildRoute";
 import useFetchData from "../../hooks/useEffectData";
 import Footer from '../../components/common/footer';
 import Button from "../../components/common/button";
-import Level1 from "../../assets/images/lv1.svg"
+
+import Level1 from "../../assets/images/lv1.svg";
+import Level2 from "../../assets/images/lv2.svg"
+import Level3 from "../../assets/images/lv3.svg"
+import Leveln from "../../assets/images/lvN.svg"
 
 export default function ProblemMain(){
   // 부모div의 버튼을 클릭했을 때 자식 div만 표시되고 부모div는 사라지게
@@ -52,17 +56,23 @@ export default function ProblemMain(){
             {data.map((problem) => (
               <div key={problem.id} className="box overflow-hidden flex-col justify-start items-start inline-flex gap-6">
                 <div className="titleContainer justify-start items-center gap-6 inline-flex">
-                  <img className='w-6 h-8' src={Level1} alt="Level Icon"></img>
+                  <img
+                    className='w-6 h-8'
+                    src={problem.level === 1 ? Level1 : problem.level === 2 ? Level2 : problem.level === 3 ? Level3 : Leveln} 
+                    alt="Level Icon"
+                  />
                   <p className='text-gray-900 text-2xl font-bold truncate'>{problem.title}</p>
                 </div>
-                <Link to={`${problem.id}`}>
-                  <Button 
-                  buttonSize="detailBtn"
-                  colorStyle="whiteBlack"
-                  content="문제 상세"
-                  onClick={() => console.log('Button clicked')}
-                  />    
-                </Link>
+                <div className='w-full flex justify-end'>
+                  <Link to={`${problem.id}`}>
+                    <Button 
+                    buttonSize="detailBtn"
+                    colorStyle="whiteBlack"
+                    content="문제 상세"
+                    onClick={() => console.log('Button clicked')}
+                    />    
+                  </Link>
+                </div>
               </div>
             ))}   
           </div>
