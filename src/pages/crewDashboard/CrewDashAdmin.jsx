e273fccc0416abf8d8119c0723adbf8a382189be
yun <90076631+yun0329@users.noreply.github.com>
2024-05-28 16:13:37 +0900

    ✨ feat: CrewDashAdmin 임시 생성

--- src/pages/crewDashboard/CrewDashAdmin.jsx
+++ src/pages/crewDashboard/CrewDashAdmin.jsx
@@ -1,9 +1,20 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
 import Footer from '../../components/common/footer';
+import CrewNav from '../../components/crewNav/crewAdmin';
+import SubHeader from "../../components/Header/subHeader";
 
-export default function crewDashAdmin(){
-  return(
+export default function CrewDashAdmin() {
+  
+  let { id } = useParams();
+
+  return (
     <div>
+      <SubHeader crewId={ id } />
+      <div className="relative mt-32">
+        <CrewNav />
+      </div>
       <Footer />
     </div>
-  )
+  );
 }
