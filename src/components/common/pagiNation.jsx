5a879c969897033ba98482de5eda6f48f9b15e04
yoojin <everywavyday@gmail.com>
2024-05-30 01:04:45 +0900

    ✨ feat: pagiNation 파일명 변경 및 기능 추가

---
+++ src/components/common/pagiNation.jsx
@@ -0,0 +1,108 @@
+import { useCallback, useState } from "react";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+
+function Button2({ content, onClick, active, disabled }) {
+  return (
+    <button
+      className={`flex flex-col cursor-pointer items-center justify-center w-10 h-10 text-sm transition-colors rounded-lg border border-gray-200
+      ${active ? "bg-color-blue-main text-white" : "text-gray-600"}
+      ${
+        !disabled
+          ? "bg-white hover:bg-color-blue-main hover:text-white"
+          : "text-color-blue-main bg-white cursor-not-allowed"
+      }
+      `}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {content}
+    </button>
+  );
+}
+
+function PaginationNav1({
+  gotoPage,
+  canPreviousPage,
+  canNextPage,
+  pageCount,
+  pageIndex,
+}) {
+  const renderPageLinks = useCallback(() => {
+    if (pageCount === 0) return null;
+    const visiblePageButtonCount = 4;
+    let numberOfButtons =
+      pageCount < visiblePageButtonCount ? pageCount : visiblePageButtonCount;
+    const pageIndices = [pageIndex];
+    numberOfButtons--;
+    [...Array(numberOfButtons)].forEach((_item, itemIndex) => {
+      const pageNumberBefore = pageIndices[0] - 1;
+      const pageNumberAfter = pageIndices[pageIndices.length - 1] + 1;
+      if (
+        pageNumberBefore >= 0 &&
+        (itemIndex < numberOfButtons / 2 || pageNumberAfter > pageCount - 1)
+      ) {
+        pageIndices.unshift(pageNumberBefore);
+      } else {
+        pageIndices.push(pageNumberAfter);
+      }
+    });
+    return pageIndices.map((pageIndexToMap) => (
+      <li key={pageIndexToMap}>
+        <Button2
+          content={pageIndexToMap + 1}
+          onClick={() => gotoPage(pageIndexToMap)}
+          active={pageIndex === pageIndexToMap}
+        />
+      </li>
+    ));
+  }, [pageCount, pageIndex]);
+  return (
+    <ul className="flex gap-2">
+      <li>
+        <Button2
+          content={
+            <div className="flex ml-1">
+              <FaChevronLeft size="0.6rem" />
+              <FaChevronLeft size="0.6rem" className="-translate-x-1/2" />
+            </div>
+          }
+          onClick={() => gotoPage(0)}
+          disabled={!canPreviousPage}
+        />
+      </li>
+      {renderPageLinks()}
+      <li>
+        <Button2
+          content={
+            <div className="flex ml-1">
+              <FaChevronRight size="0.6rem" />
+              <FaChevronRight size="0.6rem" className="-translate-x-1/2" />
+            </div>
+          }
+          onClick={() => gotoPage(pageCount - 1)}
+          disabled={!canNextPage}
+        />
+      </li>
+    </ul>
+  );
+}
+
+function PaginationNav1Presentation({data}) {
+  const [pageIndex, setPageIndex] = useState(0);
+  const itemsPerPage = 16;
+  // const pageCount = Math.ceil(data.length / itemsPerPage);
+  const pageCount = 16
+  return (
+    <div className="flex gap-3 flex-wrap justify-center py-12">
+      <PaginationNav1
+        gotoPage={setPageIndex}
+        canPreviousPage={pageIndex > 0}
+        canNextPage={pageIndex < pageCount - 1}
+        pageCount={pageCount}
+        pageIndex={pageIndex}
+      />
+    </div>
+  );
+}
+
+export { PaginationNav1Presentation };
