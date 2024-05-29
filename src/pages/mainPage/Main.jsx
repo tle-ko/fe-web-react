05b786dc9388bb696727ffaab4096a236aef08d3
yun <90076631+yun0329@users.noreply.github.com>
2024-05-29 20:27:23 +0900

    ✨ feat: mainPage 수정

--- src/pages/mainPage/Main.jsx
+++ src/pages/mainPage/Main.jsx
@@ -1,33 +1,8 @@
 import Footer from '../../components/common/footer';
-import React, { useState } from 'react';
-import Modal from '../../components/common/modal'; 
-import LanguageTag from '../../components/common/languageTag'; 
-
-
-
-const languages = [
-  'python', 'Java', 'JavaScript', 'Swift', 'C#', 'C', 'Kotlin', 'C++'
-];
 
 export default function Main() {
-  const [isModalOpen, setModalOpen] = useState(false);
-
   return (
     <div>
-      <button onClick={() => setModalOpen(true)}>언어 보기</button>
-      <Modal
-        isOpen={isModalOpen}
-        onClose={() => setModalOpen(false)}
-        title="크루 태그"
-        buttonText="닫기"
-        onButtonClick={() => setModalOpen(false)}
-      >
-        <div className="space-y-2">
-          {languages.map((language) => (
-            <LanguageTag key={language} language={language} />
-          ))}
-        </div>
-      </Modal>
       <Footer />
     </div>
   );
