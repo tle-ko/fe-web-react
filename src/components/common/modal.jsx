16329c663bd4dcdd8af8fefdd7ca8f7cb1afc28f
yun <90076631+yun0329@users.noreply.github.com>
2024-06-18 11:41:22 +0900

    🐛 fix: dropdown, modal 컴포넌트 수정

--- src/components/common/modal.jsx
+++ src/components/common/modal.jsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import close from '../../assets/images/close.svg';
 import Button from './button';
 
-const Modal = ({ isOpen, onClose, title, content, buttonText, onButtonClick }) => {
+//width,height 기본값 '80%'와 'auto'로 설정했는데 바꿔 사용할 수 있음.
+const Modal = ({ isOpen, onClose, title, content, buttonText, onButtonClick, width = '80%', height = 'auto' }) => {
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
+
+    return () => {
+      document.body.style.overflow = 'auto';
+    };
+  }, [isOpen]);
+
   if (!isOpen) return null;
 
   return (
     <div className="fixed inset-0 bg-[rgba(33,37,41,0.5)] z-50 flex justify-center items-center">
-      <div className="bg-white p-5 rounded-lg shadow-lg relative w-4/5 mx-auto max-w-[600px] max-h-[90vh] overflow-y-auto">
+      <div
+        className="bg-white p-5 rounded-lg shadow-lg relative overflow-y-auto"
+        style={{ width: width, height: height, maxHeight: '100vh' }}
+      >
         <div className="w-11/12 mx-auto">
           <div className="flex justify-between items-center mt-10">
             <h2 className="text-lg font-cafe24 font-bold text-left text-[28px]">{title}</h2>
@@ -20,12 +36,12 @@ const Modal = ({ isOpen, onClose, title, content, buttonText, onButtonClick }) =
               {content}
             </div>
             <div className="flex justify-center mt-16 mb-10">
-            <Button 
-              buttonSize="detailBtn"
-              colorStyle="blueWhite"
-              content={buttonText}
-              onClick={onButtonClick}
-            />
+              <Button 
+                buttonSize="detailBtn"
+                colorStyle="blueWhite"
+                content={buttonText}
+                onClick={onButtonClick}
+              />
             </div>
           </div>
         </div>
@@ -36,9 +52,15 @@ const Modal = ({ isOpen, onClose, title, content, buttonText, onButtonClick }) =
 
 export default Modal;
 
-// 모달 사용 예시 (content prop 전달)
-// const content = (
-//   <div>
-//     <Input title="로그인" placeholder="정보를 입력하세요." />
-//   </div>
-// );
+// 모달 사용 예시 
+//<Modal 
+//  isOpen={isModalOpen} 
+//  onClose={handleClose} 
+//  title="제목입니다" 
+//  content="내용" 
+//  buttonText="버튼 안 텍스트" 
+//  onButtonClick={handleButtonClick}
+//  width="50%" 
+//  height="60%" 
+///>
+
