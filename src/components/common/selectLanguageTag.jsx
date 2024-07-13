1c675eb35f4461e5caf21d4bb99c19b5ce2d09eb
Whatdoyumin <lym4963@naver.com>
2024-07-13 19:28:36 +0900

    ✨ feat: selectLanguageTag 컴포넌트 수정

--- src/components/common/selectLanguageTag.jsx
+++ src/components/common/selectLanguageTag.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-
+import React from "react";
+import PropTypes from 'prop-types';
 import PythonIcon from '../../assets/images/python.svg';
 import JavaIcon from '../../assets/images/java.svg';
 import JavaScriptIcon from '../../assets/images/JavaScript.svg'; 
@@ -39,44 +39,42 @@ const languageGreyIcons = {
   "C++": CPlusGreyIcon,
 };
 
-const SelectLanguageTag = ({ language }) => {
-  const [clicked, setClicked] = useState(false);
-
+const SelectLanguageTag = ({ language, onClick, selected }) => {
   const languageColor = () => {
-    if (!clicked) {
+    if (!selected) {
       return 'border border-gray-500 text-gray-600 bg-white'; 
     } else {
       switch(language) {
         case 'Python':
-          return 'bg-color-blue-pt text-gray-50';
+          return 'bg-color-blue-pt border border-color-blue-pt text-gray-50';
         case 'Java':
-          return 'bg-color-red-java text-gray-50';
+          return 'bg-color-red-java border border-color-red-java text-gray-50';
         case 'JavaScript':
-          return 'bg-color-yellow-default text-gray-50';
+          return 'bg-color-yellow-default border border-color-yellow-default text-gray-50';
         case 'Swift':
-          return 'bg-[#F05138] text-gray-50'; 
+          return 'bg-[#F05138] border border-[#F05138] text-gray-50'; 
         case 'C#':
-          return 'bg-[#512BD4] text-gray-50'; 
+          return 'bg-[#512BD4] border border-[#512BD4] text-gray-50'; 
         case 'C':
-          return 'bg-color-gray-c text-gray-50';
+          return 'bg-color-gray-c border border-color-gray-c text-gray-50';
         case 'Kotlin':
-          return 'bg-[#7F52FF] text-gray-50'; 
+          return 'bg-[#7F52FF] border border-[#7F52FF] text-gray-50'; 
         case 'C++':
-          return 'bg-color-blue-cpp text-gray-50';
+          return 'bg-color-blue-cpp border border-color-blue-cpp text-gray-50';
         default:
-          return 'bg-gray-500 text-gray-50';
+          return 'bg-gray-600 border border-gray-600 text-gray-50';
       }
     }
   };
   
   return (
     <div 
-      className={`font-pretendard font-semibold inline-flex items-center py-1 px-3 rounded-full text-sm font-medium ${languageColor()} cursor-pointer`}
-      onClick={() => setClicked(!clicked)}
+      className={`font-pretendard font-semibold inline-flex items-center py-2 px-3 rounded-full text-sm ${languageColor()} cursor-pointer`}
+      onClick={onClick}
     >
-      {(clicked ? languageIcons : languageGreyIcons)[language] && (
+      {(selected ? languageIcons : languageGreyIcons)[language] && (
         <img 
-          src={(clicked ? languageIcons : languageGreyIcons)[language]} 
+          src={(selected ? languageIcons : languageGreyIcons)[language]} 
           alt={`${language} icon`} 
           className="mr-2" 
           style={{width: '16px', height: '16px'}} 
@@ -87,7 +85,10 @@ const SelectLanguageTag = ({ language }) => {
   );
 };
 
+SelectLanguageTag.propTypes = {
+  language: PropTypes.string.isRequired,
+  selected: PropTypes.bool.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
+
 export default SelectLanguageTag;
-
-
-
