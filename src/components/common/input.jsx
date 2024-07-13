233680bee21b2e8f07989802c331a782173a2f66
Whatdoyumin <lym4963@naver.com>
2024-07-13 19:27:46 +0900

    ✨ feat: input 컴포넌트 수정

--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -1,20 +1,26 @@
-export default function Input({ title, placeholder, error, accuracy }) {
+export default function Input({ title, placeholder, width, height, error, accuracy }) {
+  const inputClassName = `px-5 py-3 bg-white rounded border border-gray-200 text-gray-800 text-base font-medium outline-none ${width ? `w-${width}` : ''} ${height ? `h-${height}` : ''}`
+
   return (
-    <div className="pt-6 flex flex-col justify-start items-start gap-2">
-      <div className="containerTitle">{title}</div>
-      <input 
-        type="text" 
-        className="px-5 py-3 bg-white rounded border border-gray-200 text-gray-800 font-medium w-full outline-none"
+    <div className="w-full flex flex-col justify-start items-start gap-2">
+      {title && <div className="containerTitle">{title}</div>}
+      <textarea
+        className={`${inputClassName} resize-none`}
         placeholder={placeholder}
+        rows="1"
+        style={{ overflowWrap: 'break-word', whiteSpace: 'pre-wrap' }}
       />
-      <div className="text-rose-500 text-sm font-semibold">{error}</div>
-      <div className="text-teal-500 text-sm font-semibold">{accuracy}</div>
+      {error && <div className="text-rose-500 text-sm font-semibold">{error}</div>}
+      {accuracy && <div className="text-teal-500 text-sm font-semibold">{accuracy}</div>}
     </div>
   )
 }
 
 /*
 usage
-<Input title="아이디" placeholder="이메일 입력" error="아이디 정보가 틀렸습니다." />
-<Input title="로그인" placeholder="아이디를 입력해주세요" accuracy="사용 가능한 아이디입니다." />
+  <Input 
+    placeholder="크루 선장에게 보낼 메시지를 입력하세요."
+    width="full"
+    height="28"
+  />
  */
