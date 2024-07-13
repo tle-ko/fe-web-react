67d718d3119342aee6819e433795cd1c50cad83e
Whatdoyumin <lym4963@naver.com>
2024-07-13 19:27:00 +0900

    ✨ feat: alertContainer(알림창) 컴포넌트 수정

--- src/components/common/alertContainer.jsx
+++ src/components/common/alertContainer.jsx
@@ -1,10 +1,17 @@
-const AlertContainer = ({alertImg, content,}) => {
-    return (
-        <div className="w-full my-10 flex-col justify-start items-center gap-6 inline-flex">
-            <img className="w-12" src={alertImg} alt='' />
-            <p className="text-gray-600 text-xl font-medium">{content}</p>
-        </div>
-  )
+import { FaCheckCircle } from "react-icons/fa";
+import { MdCancel } from "react-icons/md";
+
+const AlertContainer = ({ type, content }) => {
+  return (
+    <div className="w-full my-10 flex-col justify-start items-center gap-6 inline-flex">
+      {type === 'check' ? (
+        <FaCheckCircle color="#5383E8" size={48} />
+      ) : type === 'delete' ? (
+        <MdCancel color="#e84057" size={48} />
+      ) : null}
+      <p className="text-gray-600 text-xl font-medium">{content}</p>
+    </div>
+  );
 }
 
 export default AlertContainer;
