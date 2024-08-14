58d8be52e287f3f4f4a27dc5843f5eeb5a337531
Whatdoyumin <lym4963@naver.com>
2024-08-14 17:08:48 +0900

    💄 style: 사이드 네브 컴포넌트 hover 디자인 추가

--- src/components/nav/sideNav.jsx
+++ src/components/nav/sideNav.jsx
@@ -12,11 +12,11 @@ export default function SideNav({ elements, setSelectedElement, selectedElement 
     <div className="box w-1/4 h-fit flex-col justify-start items-start gap-4 inline-flex mr-4 whitespace-nowrap">
       {elements.map((element, index) => (
         <div key={index} 
-             className={`w-full flex-col justify-start items-start gap-3 flex cursor-pointer ${selectedElement === element.order ? 'bg-color-blue-w25 w-full text-blue-500 rounded' : 'text-black'}`}
+             className={`w-full flex-col justify-start items-start gap-3 flex cursor-pointer ${selectedElement === element.order ? 'bg-color-blue-w25 w-full text-blue-500 rounded' : 'text-black hover:text-blue-500 group'}`} // group 추가
              onClick={() => setSelectedElement(element.order)} // order로 클릭 설정
         >
-          <div className={`px-4 py-3 rounded justify-start items-start inline-flex cursor-pointer ${selectedElement === element.order ? 'text-blue-500' : 'text-black'}`}>
-            <p className="text-base font-semibold">{element.label}</p>
+          <div className={`px-4 py-3 rounded justify-start items-start inline-flex cursor-pointer ${selectedElement === element.order ? 'text-blue-500' : 'text-black group-hover:text-blue-500'}`}>
+            <p className="text-base font-semibold group-hover:text-blue-500">{element.label}</p> {/* group-hover 추가 */}
           </div>
         </div>
       ))}
