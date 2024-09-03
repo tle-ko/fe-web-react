b6125ae989207057f85b51a6809085572edd64cb
Whatdoyumin <lym4963@naver.com>
2024-09-03 19:22:16 +0900

    ✨ feat: 크루 대시보드 태그 및 멤버 목록 api 연결 및 알고리즘 그래프 임시 제거

--- src/components/crewDashboard/leftSide/leftDashboard.jsx
+++ src/components/crewDashboard/leftSide/leftDashboard.jsx
@@ -1,26 +1,34 @@
 import LanguageTag from "../../common/languageTag";
 import AlgorithmGraph from "./algorithmGraph";
 import { FaCrown } from "react-icons/fa";
+import ProfileImg from "../../../assets/images/profile.svg";
 
-export default function LeftDashboard({ crew, userData, problems }) {
-  if (!crew || !userData || !problems) return null;
+export default function LeftDashboard({ crew }) {
+  if (!crew) return null;
 
-  const getUserInfo = (userId) => userData.find(user => user.id === userId);
-
-  const crewMembers = crew.members.map(member => getUserInfo(member.user_id));
-  const host = getUserInfo(crew.host_id);
+  // 호스트와 나머지 멤버를 구분
+  const host = crew.members.find(member => member.is_captain);
+  const crewMembers = crew.members.filter(member => !member.is_captain);
 
   return (
     <div className="grid gap-6">
       <div className="box flex justify-start gap-5">
         <div className="text-gray-900 text-lg font-bold font-cafe24"><p>크루 태그</p></div>
         <div className="justify-start items-center gap-2 inline-flex flex-wrap">
-          {crew.allowed_languages.map((languageId, index) => (
-            <LanguageTag key={index} language={languageMapping[languageId]} />
+          {crew.tags
+            .filter(tag => tag.type === "language")
+            .map((tag, index) => (
+              <LanguageTag key={index} language={tag.name} />
           ))}
-          <LanguageTag language={getBojLevelTag(crew.required_boj_level)} className="tag border bg-gray-600 text-white" />
-          {crew.tags.map((tag, index) => (
-            <LanguageTag key={index} language={tag} className="bg-white text-gray-600 border border-gray-600" />
+          {crew.tags
+            .filter(tag => tag.type === "level")
+            .map((tag, index) => (
+              <LanguageTag key={index} language={tag.name} className="tag border bg-gray-600 text-white" />
+          ))}
+          {crew.tags
+            .filter(tag => tag.type === "custom")
+            .map((tag, index) => (
+              <LanguageTag key={index} language={tag.name} className="bg-white text-gray-600 border border-gray-600" />
           ))}
         </div>
       </div>
@@ -29,53 +37,25 @@ export default function LeftDashboard({ crew, userData, problems }) {
           <div className="text-gray-900 text-lg font-bold font-cafe24">
             <p>나의 동료</p>
           </div>
-          <p className="text-gray-900 text-base font-normal">{crewMembers.length}명</p>
+          <p className="text-gray-900 text-base font-normal">{crew.members.length}명</p>
         </div>
         <div className="flex flex-col gap-4">
           {host && (
             <div className="flex items-center gap-4">
-              <img src={host.image_url} alt={host.username} className="w-10 h-10 rounded-full" />
+              <img src={host.profile_image || ProfileImg} alt={host.username} className="w-10 h-10 rounded-full" />
               <span>{host.username}</span>
               <div><FaCrown color="#FFCA41" /></div>
             </div>
           )}
           {crewMembers.map((member, index) => (
-            member && member.id !== crew.host_id && (
-              <div key={index} className="flex items-center gap-4">
-                <img src={member.image_url} alt={member.username} className="w-10 h-10 rounded-full" />
-                <span>{member.username}</span>
-              </div>
-            )
+            <div key={index} className="flex items-center gap-4">
+              <img src={member.profile_image || ProfileImg } alt={member.username} className="w-10 h-10 rounded-full" />
+              <span>{member.username}</span>
+            </div>
           ))}
         </div>
       </div>
-      <AlgorithmGraph crew={crew} problems={problems} />
+      
     </div>
   );
 }
-
-const languageMapping = {
-  1005: 'Java',
-  1001: 'C',
-  1003: 'Python',
-  1004: 'C++',
-  1009: 'C#',
-  1010: 'JavaScript',
-  1013: 'Swift',
-  1008: 'Kotlin',
-};
-
-const getBojLevelTag = (level) => {
-  if (level === null) return "티어 무관";
-  const tierMapping = {
-    b: "브론즈 이상",
-    s: "실버 이상",
-    g: "골드 이상",
-    p: "플래티넘 이상",
-    d: "다이아 이상",
-    r: "루비 이상",
-    m: "마스터 이상",
-  };
-  const tier = tierMapping[level[0]];
-  return tier || "티어 무관";
-};
