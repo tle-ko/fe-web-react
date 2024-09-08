ea1bc520ce4bc6ceae6a51551275efc4be38196d
Whatdoyumin <lym4963@naver.com>
2024-09-09 00:14:07 +0900

    ✨ feat: 대시보드 왼쪽 컨테이너 멤버 프로필 이미지 경로 설정

--- src/components/crewDashboard/leftSide/leftDashboard.jsx
+++ src/components/crewDashboard/leftSide/leftDashboard.jsx
@@ -7,8 +7,8 @@ export default function LeftDashboard({ crew }) {
   if (!crew) return null;
 
   // 호스트와 나머지 멤버를 구분
-  const host = crew.members.find(member => member.is_captain);
-  const crewMembers = crew.members.filter(member => !member.is_captain);
+  const host = crew.members.items.find(member => member.is_captain);
+  const crewMembers = crew.members.items.filter(member => !member.is_captain);
 
   return (
     <div className="grid gap-6">
@@ -37,19 +37,19 @@ export default function LeftDashboard({ crew }) {
           <div className="text-gray-900 text-lg font-bold font-cafe24">
             <p>나의 동료</p>
           </div>
-          <p className="text-gray-900 text-base font-normal">{crew.members.length}명</p>
+          <p className="text-gray-900 text-base font-normal">{crew.members.count}명</p>
         </div>
         <div className="flex flex-col gap-4">
           {host && (
             <div className="flex items-center gap-4">
-              <img src={host.profile_image || ProfileImg} alt={host.username} className="w-10 h-10 rounded-full" />
+              <img src={host.profile_image ? `http://api.tle-kr.com${host.profile_image}` : ProfileImg} alt={host.username} className="w-10 h-10 rounded-full" />
               <span>{host.username}</span>
               <div><FaCrown color="#FFCA41" /></div>
             </div>
           )}
           {crewMembers.map((member, index) => (
             <div key={index} className="flex items-center gap-4">
-              <img src={member.profile_image || ProfileImg } alt={member.username} className="w-10 h-10 rounded-full" />
+              <img src={member.profile_image ? `http://api.tle-kr.com${member.profile_image}` : ProfileImg} alt={member.username} className="w-10 h-10 rounded-full" />
               <span>{member.username}</span>
             </div>
           ))}
