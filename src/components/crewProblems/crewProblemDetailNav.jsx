7cd97248b70984d0e90658ac13a5f2060ca210a2
Whatdoyumin <lym4963@naver.com>
2024-09-12 00:11:10 +0900

    ✨ feat: 크루 문제 상세 nav - 문제 제출 버튼 추가 및 url 연결

--- src/components/crewProblems/crewProblemDetailNav.jsx
+++ src/components/crewProblems/crewProblemDetailNav.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 export default function ProblemDetailNav({ problemData }) {
-  
+  const { id } = useParams();
   return (
-    <div className="w-full px-[7.5rem] py-3 left-0 bg-white border-b border-gray-200 justify-center inline-flex">
+    <div className="w-full px-[7.5rem] py-3 left-0 bg-white border-b border-gray-200 justify-center inline-flex z-10">
       <div className="flex w-full justify-between items-center">
         <div className="justify-start items-center gap-1 inline-flex">
           <Link
@@ -12,6 +12,12 @@ export default function ProblemDetailNav({ problemData }) {
             to={problemData.link}
           >
             문제 링크
+          </Link>
+          <Link 
+            className="bg-gray-50 px-4 py-3 rounded justify-center items-center flex text-gray-600 text-center text-sm font-semibold hover:text-color-blue-main hover:bg-color-blue-w25"
+            to={`/crew/${id}/problems/${problemData.id}/submit`}
+          >
+            문제 제출  
           </Link>
         </div>
       </div>
