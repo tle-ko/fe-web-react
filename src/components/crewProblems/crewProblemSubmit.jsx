62069a022bac1fda3237dd09d42c4a79f289c866
yoojin <everywavyday@gmail.com>
2024-09-26 12:31:05 +0900

    ✨ feat: 문제에 대한 코드를 전송하는 API 연결

--- src/components/crewProblems/crewProblemSubmit.jsx
+++ src/components/crewProblems/crewProblemSubmit.jsx
@@ -3,8 +3,8 @@ import { useParams, useNavigate } from 'react-router-dom';
 import CodeBox from './codeBox';
 import Button from '../../components/common/button';
 import ProblemHeader from './crewProblemHeader';
-import SubmitByHeader from './submitByHeader';
 import Dropdown from '../../components/common/dropDown';
+import { client } from '../../utils';
 
 const CrewProblemSubmit = () => {
   const navigate = useNavigate();
@@ -18,12 +18,13 @@ const CrewProblemSubmit = () => {
 
   const languageOptions = [
     'Python',
-    'Java',
-    'JavaScript',
     'C',
     'C#',
     'C++',
-    'Ruby',
+    'Java',
+    'JavaScript',
+    'Swift',
+    'Kotlin'
   ];
 
   const correctnessOptions = [
@@ -39,18 +40,37 @@ const CrewProblemSubmit = () => {
     setSelectedCorrectness(event.target.value);
   };
 
-  const handleSubmit = () => {
-    // 제출이 완료되면 URL로 이동
-    const submitId = 1; // 실제 제출 후 생성된 ID 값으로 대체 필요
-    navigate(`/crew/${id}/problems/${problemId}/submit/${submitId}`);
+  const handleSubmission = async () => {
+    const submitData = {
+      code,
+      language: selectedLanguage,
+      is_correct: selectedCorrectness === '정답'
+    };
+
+    try {
+      const response = await client.post(`/crew/activity/problem/${problemId}/submission`, submitData, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (response.status === 201) {
+        const submissionId = response.data.id; // 서버에서 반환된 제출 ID 사용
+        alert('제출이 완료되었어요!');
+        navigate(`/crew/${id}/problems/${problemId}/submission/${submissionId}`);
+      } else {
+        console.error('Submission failed:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Error submitting code:', error);
+    }
   };
+
 
   return (
     <div>
         <ProblemHeader problemId={parseInt(problemId, 10)} />
-        <SubmitByHeader id={parseInt(id, 10)} />
-
-        <div className="flex flex-col gap-6 justify-start mt-24">
+        <div className="flex flex-col gap-6 justify-start mt-10">
           <div className="inline-flex items-center gap-6">
               <div className="flex flex-col justify-start">
                 <p className="text-lg mb-3 font-semibold">언어</p>
@@ -80,7 +100,7 @@ const CrewProblemSubmit = () => {
                   buttonSize="formBtn"
                   colorStyle="blueWhite"
                   content="풀이 제출하기"
-                  onClick={handleSubmit}
+                  onClick={handleSubmission}
                 />
               </div>
             </div>
